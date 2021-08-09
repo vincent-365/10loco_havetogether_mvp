5770f088d59db5de0f274e57996c7e1058a4e31d
vincent-365 <youngjae1009@jjdream.me>
2021-08-10 02:19:42 +0900

    _FILES

--- event/uploads.php
+++ event/uploads.php
@@ -16,3 +16,26 @@ $file = $_FILES["profile"];
 print_r($profile);
 
 ?>
+<?php
+if (isset($_FILES)) {
+    $file = $_FILES["profile"];
+    // print_r($file);
+    $error = $file["error"];
+    $name = $file["name"];
+    $type = $file["type"];
+    $size = $file["size"];
+    $tmp_name = $file["tmp_name"];
+    if ( $error > 0 ) {
+        echo "Error: " . $error . "<br>";
+    }
+    else {
+      echo "Upload: " . $name . "<br>";
+      echo "Type: " . $type . "<br>";
+      echo "Size: " . ($size/1024/1024) . " Mb<br>";
+      echo "Stored in: " . $tmp_name;
+    }
+}
+else {
+    echo "File is not selected";
+}
+?>

5e4d8994df6c0ff2c780611a974d1c88331d9516
vincent-365 <youngjae1009@jjdream.me>
2021-08-10 02:25:12 +0900

    main

--- event/uploads.php
+++ event/uploads.php
@@ -17,25 +17,19 @@ print_r($profile);
 
 ?>
 <?php
-if (isset($_FILES)) {
-    $file = $_FILES["profile"];
-    // print_r($file);
-    $error = $file["error"];
-    $name = $file["name"];
-    $type = $file["type"];
-    $size = $file["size"];
-    $tmp_name = $file["tmp_name"];
-    if ( $error > 0 ) {
-        echo "Error: " . $error . "<br>";
-    }
-    else {
-      echo "Upload: " . $name . "<br>";
-      echo "Type: " . $type . "<br>";
-      echo "Size: " . ($size/1024/1024) . " Mb<br>";
-      echo "Stored in: " . $tmp_name;
-    }
-}
-else {
-    echo "File is not selected";
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
 }
 ?>

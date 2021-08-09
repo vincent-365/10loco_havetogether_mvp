566182f7ae56762bade0321ceb8f2d1372a5b5cc
vincent-365 <youngjae1009@jjdream.me>
2021-08-10 02:26:06 +0900

    Update uploads.php

--- event/uploads.php
+++ event/uploads.php
@@ -18,12 +18,12 @@ print_r($profile);
 ?>
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["profile"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  $check = getimagesize($_FILES["profile"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
